6bea01a828288b476885317a10f9de4e7ca72974
balbino aylagas <balbino_a@hotmail.com>
2022-02-02 18:56:29 -0600

    update fixed and removed comments and prints

--- db.py
+++ db.py
@@ -18,11 +18,7 @@ def insertTask(task):
     connection = createConnectionToDB()
     with connection:
         with connection.cursor() as cursor:
-            """INSERT INTO `todolistdb`.`tasks`(`id`,`description`,`is_finished`) VALUES(0,'t',0);"""
-            print(task)
             sql = "INSERT INTO `todolistdb`.`tasks`(`id`,`description`,`is_finished`) VALUES(%s,%s,%s);"
             data = (task.id, task.description, int(task.isFinished))
-            print(data)
-            print(type(data))
             cursor.execute(sql, data)
         connection.commit()
